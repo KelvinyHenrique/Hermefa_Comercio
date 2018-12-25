094021a879c6624dc8e42481b6a3fc0852481c70
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2018-12-25 11:04:56 -0200

    Ajusando upload de imagem

--- admin/php/enviarprodutos.php
+++ admin/php/enviarprodutos.php
@@ -5,30 +5,30 @@
     $descricao = $_POST['descricao'];
     $codigo = $_POST['codigo'];
    //$categoria = $_POST['categoria'];
-    $foto = $_FILE['foto'];
+    $foto = $_FILES['foto'];
     print_r($_REQUEST);
     //Define o Caminho
-    $destino = "../../assets/images/produtos/";
+    $destino = "../assets/images/produtos/";
 
     //  Define um nome para a imagem
     $nome = md5(uniqid(time())).".png";
-    
+     
     //Armazena na variavel o novo nome
-    $arquivo = basename($foto['$nome']);
+    $arquivo = basename($nome);
     
     //concatena a variavel do caminho com o nome do arquivo 
     $destino = $destino.$arquivo; 
     
-    //testa se a funçao de upload rodar
-if(move_uploaded_file($foto['tmp_name'], $destino)) {
-
+  //testa se a funçao de upload rodar
+if(move_uploaded_file($nome['tmp_name'], $destino)) {
+    $sql = "INSERT INTO `produtos`( `nome`, `descricao`, `codigo`, 'imagem') VALUES ('$produto', '$descricao', '$codigo', '$destino')";
+    $sql = $conexao->query($sql);
     echo "O arquivo $arquivo foi enviado.";
 } else{
     echo "Ocorreu um erro. Tente novamente";
 }
 
-    $sql = "INSERT INTO `produtos`( `nome`, `descricao`, `codigo`, 'imagem') VALUES ('$produto', '$descricao', '$codigo', '$destino')";
-    $sql = $conexao->query($sql);
+  
 
 
 
