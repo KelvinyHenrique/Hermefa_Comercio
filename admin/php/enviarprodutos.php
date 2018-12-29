d3d0a6f50ddc7e533101eed9d68d73211430c33e
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2018-12-28 23:47:29 -0200

    Terminando css do site

--- admin/php/enviarprodutos.php
+++ admin/php/enviarprodutos.php
@@ -9,7 +9,7 @@
     
     if(isset($imagem['tmp_name']) && !empty($imagem['tmp_name'])) {
          $nomearquivo = md5(time().rand(0,1000)).'.png';
-         move_uploaded_file($imagem['tmp_name'], '../../assets/images/produtos/'.$nomearquivo);
+         move_uploaded_file($imagem['tmp_name'], 'assets/images/produtos/'.$nomearquivo);
          
          $destino = "../../assets/images/produtos/".$nomearquivo;
          $sql = "INSERT INTO `produtos`( `nome`, `descricao`, `codigo`, `categoria`, `imagem`) VALUES ('$produto', '$descricao', '$codigo', '$select', '$destino')";
--- index.php
+++ index.php
@@ -146,12 +146,11 @@
     </div>
   </div>
 
-  <div class="conteiner">
-      <div class="bannercloseup"><img src="assets/images/bannercloseup.png" alt=""></div>
+<div class="conteiner">
+      <div class="bannercloseup"></div>
   </div>
-
-
       <div class="conteiner">
+      
           <div class="bannerdove">
       <div class="banner1direita"><img src="assets/images/Dove656.png" alt=""></div>
       <div class="bannerdir">
@@ -169,10 +168,85 @@
         <h1><span>Outros Destaques</span></h1>
       </div>
       <div class="linhahorizontal"></div>
+    </div>
+  </div>
+
+  <div class="conteiner">
+    <div class="conteudosprodutos">
+
+      <?php 
+      require "assets/php/config.php";
+      $sql = "SELECT * FROM produtos ORDER BY id DESC"; 
+      $sql = $conexao->query($sql);
+      if($sql->rowCount() > 0) {
+        foreach($sql->fetchAll() as $produto):
+      ?>
+      <div class="produto">
+        <img src="<?php echo $produto['imagem'];?>">
+        <p>
+          <?php echo $produto['nome'];?>
+        </p>
+        <p>
+          <?php echo $produto['codigo'];?>
+        </p>
+      </div>
+      <?php endforeach;
+      } else {
+        echo "Nenhum produto cadastrado";
+      } ?>
 
     </div>
   </div>
 
+
+  <div class="conteiner">
+  <div class="rodape">
+  <div class="contentrodape">
+        <div class="item1rodape">
+          <img src="assets/images/logo.png" alt="">
+          <h5>A Hermefa Distribuidor Atacadista S/A é uma empresa especializada em cosmético e higiene pessoal.</h5>
+          <button>Saiba Mais</button>
+        </div>
+        <div class="contentfooterdir">
+        <div class="item2rodape">
+          <h4>Call Center</h4>
+          <a href="">0800-123-4567</a><br/>
+          <a href="">0800-123-4567</a><br/>
+          <a href="">0800-123-4567</a><br/>
+
+        </div>
+
+        <div class="item3rodape">
+          <h4>Sac</h4>
+          <a href="">0800-123-4567</a><br/>
+          <a href="">0800-123-4567</a><br/>
+          <p>Fale Conosco</p>
+          <a href="">0800-123-4567</a>
+        </div>
+
+        <div class="item4rodape">
+          <h4>Institucional</h4>
+          <a href="">Fale Conosco</a><br/>
+          <a href="">Trabalhe Conosco</a><br/>
+          <a href="">Fale Conosco</a><br/>
+           </div>
+
+      <div class="item5rodape">
+        <h4>Serviços</h4>
+        <a href="">Extranet</a><br/>
+          <a href="">Pedido Eletrônico</a><br/>
+          <a href="">Webmail</a><br/>
+      </div>
+        </div>
+       
+          
+     
+
+
+
+      </div>
+  </div>
+</div>
 
 </body>
 
